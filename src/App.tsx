faf76c000754b888b7871d749067c5ae95995773
Luka Tutek <luka.tutek@swisscom.com>
2024-05-08 14:02:29 +0200

    feat: started on introduction further adjustments and thoughts are needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ProjectComponent from "./Components/ProjectComponent/project.component";
 import HeaderComponent from "./Components/HeaderComponent/header.component";
+import IntroductionComponent from "./Components/IntroductionComponent/introduction.component";
 
 function App() {
   return (
     <>
       <HeaderComponent />
+      <IntroductionComponent />
     </>
   );
 }
--- src/Components/IntroductionComponent/introduction.component.tsx
+++ src/Components/IntroductionComponent/introduction.component.tsx
@@ -1,8 +1,28 @@
+import "./introduction.component.css";
+import astronaut_background from "../../Images/astronaut_background.jpg";
 function IntroductionComponent() {
   return (
-    <div>
-      <h1>Welcome to the Introduction Component</h1>
-    </div>
+    <>
+      <div className="introduction-container"></div>
+      <div className="cardContaienr">
+        <div className="col-lg-6 col-xl-5 mag-5">
+          <sdx-card
+            label="Title"
+            label-aria-level="3"
+            image-src={astronaut_background}
+            image-alt="austronaut image"
+            href="javascript:;"
+            href-label="Link"
+            href-aria-label="Open new website."
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod
+            </p>
+          </sdx-card>
+        </div>
+      </div>
+    </>
   );
 }
 
