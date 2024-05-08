fd22f3b074125fc48f68e8d40a8918e6e55826a7
Luka Tutek <luka.tutek@swisscom.com>
2024-05-08 12:03:40 +0200

    feat: Header logo & info pannel available

--- src/Components/HeaderComponent/header.component.tsx
+++ src/Components/HeaderComponent/header.component.tsx
@@ -4,7 +4,7 @@ function HeaderComponent() {
       <sdx-header
         index={{
           label: "Home",
-          href: "https://intranet.swisscom.com/home/profile/i:0e.t%7Cazuread%7Ctaavabr1",
+          href: "/",
         }}
         slots='[
         {
@@ -23,37 +23,25 @@ function HeaderComponent() {
 
         
       ]'
-        navigation='{
-        "tertiary": {
-          "children": [
-            {
-              "iconName": "icon-network",
-              "label": "Intranet",
-              "href": "https://intranet.swisscom.com/home/profile/taatulu3/profile"
-            },
-            {
-              "iconName": "icon-linkedin",
-              "label": "LinkedIn",
-              "href": "https://www.linkedin.com/in/luka-tutek-b14850279/"
-            }
-          ]
-        }
-      }'
       >
         <div slot="contact" className="container">
           <div className="row">
-            <div className="col-10 padding-bottom-4">
+            <div className="col-10 ">
               <sdx-button
                 theme="transparent"
+                label="LinkedIn"
                 icon-name="icon-linkedin"
                 href="https://www.linkedin.com/in/luka-tutek-b14850279/"
-                icon-size="2"
+                icon-size="3"
               ></sdx-button>
+            </div>
+            <div className="col-10">
               <sdx-button
                 theme="transparent"
+                label="Intranet"
                 icon-name="icon-network"
                 href="https://intranet.swisscom.com/home/profile/taatulu3/profile"
-                icon-size="2"
+                icon-size="3"
               ></sdx-button>
             </div>
           </div>
