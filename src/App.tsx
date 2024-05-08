3df73d5900e05ad2a7799f10526fab45ca6a3f03
Luka Tutek <luka.tutek@swisscom.com>
2024-05-08 11:14:40 +0200

    feat: header start WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import ProjectComponent from "./Components/ProjectComponent/project.component";
+import HeaderComponent from "./Components/HeaderComponent/header.component";
 
 function App() {
   return (
     <>
-      <ProjectComponent />
+      <HeaderComponent />
     </>
   );
 }
--- src/Components/HeaderComponent/header.component.tsx
+++ src/Components/HeaderComponent/header.component.tsx
@@ -1,8 +1,65 @@
 function HeaderComponent() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <>
+      <sdx-header
+        index={{
+          label: "Home",
+          href: "https://intranet.swisscom.com/home/profile/i:0e.t%7Cazuread%7Ctaavabr1",
+        }}
+        slots='[
+        {
+          "label": "Download CV",
+          "iconName": "icon-download-cloud",
+          "slot": "download",
+          "scrollable": false,
+          "href": "javascript:;"      
+        },
+        {
+          "label": "Contact",
+          "iconName": "icon-information-circle",
+          "slot": "contact",
+          "scrollable": false
+        }
+
+        
+      ]'
+        navigation='{
+        "tertiary": {
+          "children": [
+            {
+              "iconName": "icon-network",
+              "label": "Intranet",
+              "href": "https://intranet.swisscom.com/home/profile/taatulu3/profile"
+            },
+            {
+              "iconName": "icon-linkedin",
+              "label": "LinkedIn",
+              "href": "https://www.linkedin.com/in/luka-tutek-b14850279/"
+            }
+          ]
+        }
+      }'
+      >
+        <div slot="contact" className="container">
+          <div className="row">
+            <div className="col-10 padding-bottom-4">
+              <sdx-button
+                theme="transparent"
+                icon-name="icon-linkedin"
+                href="https://www.linkedin.com/in/luka-tutek-b14850279/"
+                icon-size="2"
+              ></sdx-button>
+              <sdx-button
+                theme="transparent"
+                icon-name="icon-network"
+                href="https://intranet.swisscom.com/home/profile/taatulu3/profile"
+                icon-size="2"
+              ></sdx-button>
+            </div>
+          </div>
+        </div>
+      </sdx-header>
+    </>
   );
 }
 
