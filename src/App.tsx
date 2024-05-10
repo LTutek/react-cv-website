7170183f0e222951da8252f77a91ecb56de6076d
Luka Tutek <luka.tutek@swisscom.com>
2024-05-10 14:58:58 +0200

    feat: added title and some extra things idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import ProjectComponent from "./Components/ProjectComponent/project.component";
 import HeaderComponent from "./Components/HeaderComponent/header.component";
+import HobbieComponent from "./Components/HobbieComponent/hobbie.component";
 
 function App() {
   return (
     <>
       <HeaderComponent />
       <ProjectComponent />
+      <HobbieComponent />
     </>
   );
 }
--- src/Components/HobbieComponent/hobbie.component.tsx
+++ src/Components/HobbieComponent/hobbie.component.tsx
@@ -1,13 +1,31 @@
+import "./hobbie.component.css";
 function HobbieComponent() {
   return (
-    <div>
-      <h1>Hobbies</h1>
-      <ul>
-        <li>Reading</li>
-        <li>Writing</li>
-        <li>Swimming</li>
-      </ul>
-    </div>
+    <>
+      <h1 className="hobbieTitle">Hobbies</h1>
+      <sdx-tabs style={{ marginTop: "20px" }}>
+        <sdx-tabs-item label="Fitness" icon-name="icon-fitness">
+          Das Fitnessstudio ist für mich ein Ort, an dem ich nach der Arbeit
+          oder der Schule abschalten kann. Ich betrachte es als eine äusserst
+          wichtige Freizeitaktivität.
+        </sdx-tabs-item>
+        <sdx-tabs-item label="Travel" icon-name="icon-suitcase">
+          Reisen ist meine Leidenschaft, und ich strebe danach, so viele Orte
+          wie möglich zu besuchen und zu erkunden.
+        </sdx-tabs-item>
+        <sdx-tabs-item label="Friends" icon-name="icon-crowd">
+          An den Wochenenden treffe ich mich sehr gerne mit Freunden, um
+          gemeinsam Zeit zu verbringen und schöne Momente zu erleben.
+        </sdx-tabs-item>
+        <sdx-tabs-item label="Gaming" icon-name="icon-gaming">
+          In meiner Freizeit zu Hause spiele ich gerne Videospiele mit Freunden,
+          um abzuschalten und Spass zu haben.
+        </sdx-tabs-item>
+        <sdx-tabs-item label="Reading" icon-name="icon-book">
+          smth.
+        </sdx-tabs-item>
+      </sdx-tabs>
+    </>
   );
 }
 
