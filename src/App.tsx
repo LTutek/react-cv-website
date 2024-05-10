ca8f5dcf670c00f84c59b0d19b76aaa2765861cc
Luka Tutek <luka.tutek@swisscom.com>
2024-05-10 13:01:30 +0200

    feat: Filtering the technology data works styling is needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import ProjectComponent from "./Components/ProjectComponent/project.component";
 import HeaderComponent from "./Components/HeaderComponent/header.component";
+import TechnologyComponent from "./Components/TechnologyComponent/technology.component";
 
 function App() {
   return (
     <>
       <HeaderComponent />
       <ProjectComponent />
+      <TechnologyComponent />
     </>
   );
 }
--- src/Components/TechnologyComponent/technology.component.tsx
+++ src/Components/TechnologyComponent/technology.component.tsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import { technologies } from "./technology-data";
+
 function TechnologyComponent() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("frontend");
+  const [filteredTechnologies, setFilteredTechnologies] =
+    useState(technologies);
+
+  useEffect(() => {
+    const filterTechnologies = () => {
+      const filtered = technologies.filter((tech) =>
+        tech.category.includes(selectedCategory)
+      );
+      setFilteredTechnologies(filtered);
+    };
+
+    filterTechnologies();
+  }, [selectedCategory]);
+
+  const handleCategoryChange = (
+    event: React.FormEvent<HTMLSdxInputGroupElement>
+  ) => {
+    const target = event.target as HTMLInputElement;
+    setSelectedCategory(target.value);
+  };
+
   return (
-    <div>
-      <h1>Technology</h1>
-    </div>
+    <>
+      <sdx-input-group
+        type="radio"
+        label="Radio group (inline)"
+        inline
+        onInput={handleCategoryChange}
+      >
+        <sdx-input-item
+          value="frontend"
+          checked={selectedCategory === "frontend"}
+        >
+          Frontend
+        </sdx-input-item>
+        <sdx-input-item
+          value="backend"
+          checked={selectedCategory === "backend"}
+        >
+          Backend
+        </sdx-input-item>
+        <sdx-input-item value="tools" checked={selectedCategory === "tools"}>
+          Tools
+        </sdx-input-item>
+      </sdx-input-group>
+      <div className="technology-container">
+        {filteredTechnologies.map((technology, index) => (
+          <div key={index} className="technology-card">
+            <img
+              src={technology.logo}
+              alt={technology.name}
+              className="technology-logo"
+            />
+            <h3>{technology.name}</h3>
+            <p>{technology.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
